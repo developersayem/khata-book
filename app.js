42b290d3a459114cac81c18ddd44f555a43d7981
Molla Sayem <developersayem012@gmail.com>
2025-01-13 16:34:06 +0600

    edit,view and delete route added

--- app.js
+++ app.js
@@ -22,12 +22,55 @@ app.get("/create", (req, res) => {
 
 app.post("/createhisaab", (req, res) => {
   const currentDate = new Date();
-  const date = `${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}-${currentDate.getMilliseconds()}`;
+  const date = `${currentDate.getDate()}-${
+    currentDate.getMonth() + 1
+  }-${currentDate.getFullYear()}-${currentDate.getSeconds()}`;
 
-  fs.writeFile("./hisaab/" + date, req.body.content, (err) => {
+  fs.writeFile("./hisaab/" + date + ".txt", req.body.content, (err) => {
     if (err) return res.status(500).send("Something went wrong");
 
     res.redirect("/");
   });
 });
+
+app.get("/hisaab/:filename", (req, res) => {
+  fs.readFile("./hisaab/" + req.params.filename, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Something went wrong");
+
+    res.status(200).render("hisaab", { data, filename: req.params.filename });
+  });
+});
+
+app.get("/edit/:filename", (req, res) => {
+  fs.readFile("./hisaab/" + req.params.filename, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Something went wrong");
+
+    res.status(200).render("edit", { data, filename: req.params.filename });
+  });
+});
+
+app.post("/update/:filename", (req, res) => {
+  fs.writeFile(
+    "./hisaab/" + req.params.filename.split(":")[1],
+    req.body.content,
+    (err) => {
+      if (err)
+        return res.status(500).send("Something went wrong: " + err.message);
+
+      // Redirect to the "hisaab" route to display updated content
+      res.status(200).render("hisaab", {
+        data: req.body.content,
+        filename: req.params.filename.split(":")[1],
+      });
+    }
+  );
+});
+app.get("/delete/:filename", (req, res) => {
+  fs.unlink("./hisaab/" + req.params.filename, (err) => {
+    if (err) return res.status(500).send("Something went wrong");
+
+    res.redirect("/");
+  });
+});
+
 app.listen(3000);
